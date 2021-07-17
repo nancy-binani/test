8ec1b779d22ad61543fccbb0f4aed9586093d379
Nancy Binani <nancybinani1207@gmail.com>
2021-07-17 20:41:13 +0530

    code

---
+++ src/Category.jsx
@@ -0,0 +1,7 @@
+import react from 'react'
+class React extends React.Component{
+    state = {
+        "animal" : ["dog","bird","reptile","rabbit"],
+        "breed" : ["Havanese","Goldendoodle", "Boxer","Wheaten Terrier", "Cockatoo","Horned Lizard","Shih Tzu","American","Labrador"]
+    }
+}
